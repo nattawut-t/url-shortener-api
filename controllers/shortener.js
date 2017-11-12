210d6e7bc04db16c64b821b073d4d740552b1f4f
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-12 22:04:50 +0700

    connect controller to db

--- controllers/shortener.js
+++ controllers/shortener.js
@@ -1,11 +1,40 @@
-// const Boom = require('boom')
+const Boom = require('boom')
+const Url = require('../models/url')()
 
-const shorten = (request, reply) => {
+const shorten = async (request, reply) => {
   const { payload: { url } } = request
 
   console.log(url)
 
-  return reply({ url: 'mockurl' })
+  try {
+    let shortUrl
+    const existingUrl = await Url
+      .findOne({ longUrl: url })
+      .select({
+        id: 1,
+        shortUrl: 1,
+      })
+      .exec()
+
+    console.log('existingUrl: ', existingUrl)
+
+    if (!existingUrl) {
+      const _url = new Url()
+      _url.longUrl = url
+      _url.shortUrl = shortUrl
+      await _url.save()
+
+      shortUrl = _url.id
+    } else {
+      shortUrl = existingUrl.id
+    }
+
+    return reply({ url: shortUrl })
+  } catch (error) {
+    console.log('shorten.error: ', error)
+    return reply(Boom.wrap(new Error('Internal Server Error'), 500))
+  }
+
 }
 
 const getUrl = (request, reply) => {
